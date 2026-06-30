734aeaa16fca84436464176dddac0c5ac0d96063
sadikgp3-cmyk <sadikgp3@gmail.com>
2026-06-30 10:12:17 +0600

    Update db_manager.py

--- db_manager.py
+++ db_manager.py
@@ -1,1 +1,10 @@
 
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect('numbers.db')
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS user_numbers 
+                      (id INTEGER PRIMARY KEY, user_id INTEGER, service TEXT, data TEXT)''')
+    conn.commit()
+    conn.close()
